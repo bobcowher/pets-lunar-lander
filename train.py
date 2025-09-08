d1e876a1d9e64b8f539d2a1e283bbe3b51e34141
Robert Cowher <bobcowher@gmail.com>
2025-09-08 17:21:27 -0500

    Adding a timer

--- train.py
+++ train.py
@@ -13,6 +13,8 @@ max_memory_size = 100000
 episodes = 500
 dynamics_model_batch_size = 256 
 
+start_time = time.perf_counter()
+
 env = gym.make("LunarLander-v3", continuous=True, gravity=-10.0,
                enable_wind=False, wind_power=15.0, turbulence_power=1.5,
                render_mode="rgb_array")
@@ -20,5 +22,11 @@ env = gym.make("LunarLander-v3", continuous=True, gravity=-10.0,
 agent = Agent(env=env)
 
 agent.train(episodes=episodes)
+
+end_time = time.perf_counter()
+
+elapsed_time = end_time - start_time
+
+print(f"Elapsed time was: {elapsed_time}")
         
 
